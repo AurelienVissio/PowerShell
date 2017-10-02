18198aa567aa2e55615bae86a3aec8c8c038292d
Aurelien Vissio <aurelien.vissio@roh.org.uk>
2017-10-02 18:03:24 +0100

    Added help

--- Set-ROHLeaver/Set-ROHLeaver.ps1
+++ Set-ROHLeaver/Set-ROHLeaver.ps1
@@ -1,4 +1,17 @@
 function Set-ROHLeaver {
+    <#
+    .SYNOPSIS
+    Render AD attributes void
+    
+    .DESCRIPTION
+    The script will look for specified atrributes with values and amend said values to a null state.
+    
+    .EXAMPLE
+    
+    
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
         param (
             
